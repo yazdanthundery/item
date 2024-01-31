f603e6423744900e4a0de35a7c96c91b37486814
unknown <hunters.cainhurst@gmail.com>
2024-01-31 23:19:57 +0330

    first

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const ps5img = document.getElementById("colors");
+const whiteps5 = document.getElementById("white");
+const blackps5 = document.getElementById("black");
+
+whiteps5.addEventListener("click", function(){
+    ps5img.src = "./../img/92e6aa40dbbc114d0ea7f56403dae94503f885f1_1609679750.jpg";
+});
+blackps5.addEventListener("click", function(){
+    ps5img.src = "./../img/PS5-Black.jpg";
+});
